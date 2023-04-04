1ebca2a0b651f61af46ba02d0761df83cf9f1133
haytham5 <78439856+haytham5@users.noreply.github.com>
2023-04-04 13:16:59 -0400

    Working on scan radius

--- localguide/src/Components/LocalGuide.js
+++ localguide/src/Components/LocalGuide.js
@@ -1,12 +1,23 @@
 import './localguide.css';
 import { useState } from 'react';
 import statusBar from './Status Bar.png';
-import { Button } from '@mui/material';
-
+import { Button, IconButton } from '@mui/material';
+import Wifi1BarIcon from '@mui/icons-material/Wifi1Bar';
+import Wifi2BarIcon from '@mui/icons-material/Wifi2Bar';
+import WifiIcon from '@mui/icons-material/Wifi';
 
 function LocalGuide() {
     const [showLanding, setShowLanding] = useState(true);
     const [showHome, setShowHome] = useState(false);
+
+    const [scanSize, setScanSize] = useState("Small");
+    
+    const increaseSize = () => {
+        if(scanSize === "Small") setScanSize("Medium");
+        if(scanSize === "Medium") setScanSize("Large");
+        if(scanSize === "Large") setScanSize("Small");
+
+    }
 
     const landingToHome = () => {
         setShowLanding(false);
@@ -35,9 +46,32 @@ function LocalGuide() {
             { showHome &&
             <div className="MainPage">
                 <img src={statusBar} className="StatusBar" alt="Status Bar"/>
-                <div className='Circle2'></div>
-                <div className='Circle1'></div>
 
+                <Button variant="contained" endIcon={
+                    scanSize === "Small" ? <Wifi1BarIcon /> : scanSize === "Medium" ? <Wifi2BarIcon /> : <WifiIcon /> 
+                
+                } onClick={increaseSize}>{scanSize}</Button>
+                
+                {scanSize == "Small" &&
+                    <div>
+                        <div className='Circle1' style={{width: '100px', height: '100px'}}></div>
+                        <div className='Circle2' style={{width: '110px', height: '110px'}}></div>
+                    </div>
+                }
+
+                {scanSize == "Medium" && 
+                    <div>
+                        <div className='Circle1' style={{width: '200px', height: '200px'}}></div>
+                        <div className='Circle2' style={{width: '205px', height: '205px'}}></div>
+                    </div>
+                }
+
+                {scanSize == "Large" && 
+                    <div>
+                        <div className='Circle1' style={{width: '285px', height: '285px'}}></div>
+                        <div className='Circle2' style={{width: '290px', height: '290px'}}></div>
+                    </div>
+                }
             </div>}
         </div>
     );
