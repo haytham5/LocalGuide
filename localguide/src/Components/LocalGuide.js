f2f718b92c15f7313f2fdd04b9f1a0077876c791
haytham5 <haythamhnine5@gmail.com>
2023-04-05 17:32:32 -0400

    Adding animations

--- localguide/src/Components/LocalGuide.js
+++ localguide/src/Components/LocalGuide.js
@@ -1,14 +1,38 @@
 import './localguide.css';
 import { useState } from 'react';
 import statusBar from './Status Bar.png';
-import { Button, IconButton } from '@mui/material';
+import { Button, IconButton, Modal, Box } from '@mui/material';
 import {Wifi1Bar, Wifi2Bar, Wifi, Restaurant, AddCircle, Hotel, Park, FmdGood  } from '@mui/icons-material';
-import { useTheme } from '@mui/material/styles';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
+import Rating from '@mui/material/Rating';
+import Grow from '@mui/material/Grow';
+import Zoom from '@mui/material/Zoom';
+
+
+
+const scanDiv = {
+    paddingTop: '90%', 
+    height: '100%', 
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 210,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '5%'
+};
 
 function LocalGuide() {
     const [showLanding, setShowLanding] = useState(false);
@@ -20,10 +44,40 @@ function LocalGuide() {
     const [pinClicked, setPinClicked] = useState(false);
     const [currentData, setCurrentData] = useState({});
 
-    const data = [{name: 'restaurant'}, {name: 'wrong-staurant'}];
+    const data = [
+        {
+            image: '',
+            name: 'Pastramis',
+            type: "Restaurant",
+            addTypeInfo: 'Italian Restaurant',
+            address: '8302, Italian street',
+            rating: 4,
+            review: 'This is a pretty sweet spot',
+            distance: '5km',
+        }, 
+        {
+            image: '',
+            name: 'Chez Ping',
+            type: "Restaurant",
+            addTypeInfo: 'Asian Restaurant',
+            address: '8302, Asian street',
+            rating: 5,
+            review: 'This is a pretty great place',
+            distance: '1km',
+        }, 
+        {
+            image: '',
+            name: 'Box',
+            type: "Restaurant",
+            addTypeInfo: 'Not a Restaurant',
+            address: '8302, side of road',
+            rating: 1,
+            review: 'sucks',
+            distance: '100m',
+        }, 
+    ];
 
     {/* TODO: change bottombar border based on what is clicked */}
-        {/* TODO: Align Card from Bottom */}
 
 
     const clickPin = (id) => {
@@ -36,10 +90,6 @@ function LocalGuide() {
 
         }
     }
-    
-    const theme = useTheme();
-
-
 
     const clickR = () => {
         if(restaurantClicked) setRestaurantClicked(false);
@@ -106,49 +156,85 @@ function LocalGuide() {
                 <Button variant="contained" endIcon={
                     scanSize === "Small" ? <Wifi1Bar /> : scanSize === "Medium" ? <Wifi2Bar /> : <Wifi /> 
                 
-                } onClick={increaseSize}>{scanSize}</Button>
+                } onClick={increaseSize} sx={{marginTop: '10px'}}>{scanSize}</Button>
                 
                 {/* SCAN RADIUS */}
                 {scanSize === "Small" &&
-                    <div>
-                        <div className='Circle1' style={{width: '100px', height: '100px'}}></div>
-                        <div className='Circle2' style={{width: '105px', height: '105px'}}></div>
-                    </div>
+                    <Box sx={{ display: 'flex' }}>
+                        <Grow
+                        in={scanSize === "Small"}
+                        sx={{transformOrigin: 'bottom'}}
+                        {...(scanSize === "Small" ? { timeout: 700 } : {})}
+                        >
+                            <div style={scanDiv}>
+                                <div className='Circle1' style={{width: '100px', height: '100px'}}></div>
+                                <div className='Circle2' style={{width: '105px', height: '105px'}}></div>
+                            </div>
+                        </Grow>
+                    </Box>
                 }
 
                 {scanSize === "Medium" && 
-                    <div>
-                        <div className='Circle1' style={{width: '200px', height: '200px'}}></div>
-                        <div className='Circle2' style={{width: '205px', height: '205px'}}></div>
-                    </div>
+                    <Box sx={{ display: 'flex' }}>
+                        <Grow
+                        in={scanSize === "Medium"}
+                        sx={{transformOrigin: 'bottom'}}
+                        {...(scanSize === "Medium" ? { timeout: 700 } : {})}
+                        >
+                            <div style={scanDiv}>
+                                <div className='Circle1' style={{width: '200px', height: '200px'}}></div>
+                                <div className='Circle2' style={{width: '205px', height: '205px'}}></div>
+                            </div>
+                        </Grow>
+                    </Box>
                 }
 
                 {scanSize === "Large" && 
-                    <div>
-                        <div className='Circle1' style={{width: '285px', height: '285px'}}></div>
-                        <div className='Circle2' style={{width: '290px', height: '290px'}}></div>
-                    </div>
+                    <Box sx={{ display: 'flex' }}>
+                        <Grow
+                        in={scanSize === "Large"}
+                        sx={{transformOrigin: 'bottom'}}
+                        {...(scanSize === "Large" ? { timeout: 700 } : {})}
+                        >
+                            <div style={scanDiv}>
+                                <div className='Circle1' style={{width: '285px', height: '285px'}}></div>
+                                <div className='Circle2' style={{width: '290px', height: '290px'}}></div>
+                            </div>
+                        </Grow>
+                    </Box>
                 }
+                
 
                 {/* PINS */}
                 {/* RESTAURANTS */}
                 {restaurantClicked && 
-                    <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '355px', left: '125px'}} size="large" 
-                        onClick={() => clickPin(0)}
+                    <Grow
+                    in={restaurantClicked}
+                    {...(restaurantClicked ? { timeout: 700 } : {})}
                     >
-                        <FmdGood />
-                    </IconButton>                
+                        <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '355px', left: '125px'}} size="large" 
+                        onClick={() => clickPin(0)}>
+                            <FmdGood />
+                        </IconButton>      
+                    </Grow>
                 }
 
                 {restaurantClicked && scanSize !== 'Small' &&
-                    <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '260px', left: '190px'}} size="large" 
-                    onClick={() => clickPin(1)}>
-                        <FmdGood />
-                    </IconButton>
+                    <Grow
+                    in={restaurantClicked && scanSize !== 'Small'}
+                    {...(restaurantClicked && scanSize !== 'Small' ? { timeout: 700 } : {})}
+                    >
+                        <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '260px', left: '190px'}} size="large" 
+                        onClick={() => clickPin(1)}>
+                            <FmdGood />
+                        </IconButton>
+                    </Grow>
+
                 }
 
                 {restaurantClicked && scanSize === 'Large' &&
-                    <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '450px', left: '65px'}} size="large" >
+                    <IconButton color="secondary" sx={{color: '#064789',position: 'absolute', bottom: '450px', left: '65px'}} size="large" 
+                    onClick={() => clickPin(2)}>
                         <FmdGood />
                     </IconButton>
                 }
@@ -173,33 +259,40 @@ function LocalGuide() {
                     </IconButton>
                 }
 
-                {/* INFO POPUP TODO MAKE THIS A MODAL*/}
-                {pinClicked &&
-                    <Card sx={{ maxWidth: 345 }}>
+                <Modal open={pinClicked} onClose={()=> setPinClicked(false)}>
+                    <Box sx={style}>
                         <CardMedia
                             component="img"
-                            alt="green iguana"
-                            height="140"
-                            image="/static/images/cards/contemplative-reptile.jpg"
+                            alt=""
+                            height="90"
+                            image=""
                         />
 
                         <CardContent align='left' >
-                            <Typography gutterBottom variant="h5" component="div">
-                                Lizard
+                            <Typography variant="h5" component="p">
+                                {currentData.name}
                             </Typography>
 
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
+                            <Typography variant="h6" component="p">
+                                {currentData.addTypeInfo}
+                            </Typography>
+
+                            <Typography gutterBottom variant="subtitle1" color="text.secondary">
+                                <strong>{currentData.address}</strong>, <em>{currentData.distance}</em>
+                            </Typography>
+
+                            <Rating value={currentData.rating} readOnly/>
+                            
+                            <Typography gutterTop variant="body1" color="text.secondary">
+                                <em>"{currentData.review}"</em>
                             </Typography>
                         </CardContent>
 
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
+                        <CardActions >
+                            <Button size="small" onClick={()=> setPinClicked(false)}>Close</Button>
                         </CardActions>
-                    </Card>
-                }
+                    </Box>
+                </Modal>
 
                 {/* BUTTONS */}
                 <div className="bottomBar">
